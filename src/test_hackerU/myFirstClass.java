25400176f94068f2b0ef88fc52f596dd056780f9
MineevDS_PC <MineevDS_PC@10.100.102.6>
2022-04-03 19:31:17 +0300

    Create new project

---
+++ src/test_hackerU/myFirstClass.java
@@ -0,0 +1,12 @@
+package test_hackerU;
+
+public class myFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello!!!!");
+
+	}
+
+}
